42c705ad4c7b4ef6dc7d4ec18cf6fc3cd5f0bf04
ricardoolmeda <richard250cc@gmail.com>
2024-12-05 11:24:08 +0100

    Actualización de la tabla de tipos y la hemos separado

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Importamos la libreria requests
 import requests
+import my_module
 
 # Creamos la variable URL con nuestro enlace
 URL = "https://pokeapi.co/api/v2/pokemon/"
@@ -43,48 +44,6 @@ for types in datos["types"]:
 # Quiero que me indique las debilidades que tiene el pokemon para ello creamos un diccionario
 
 # Diccionario con fortalezas, debilidades e inmunidades de cada tipo de Pokémon
-tipos_pokemons = {
-    "Water": {
-        "strengths": {"Fire": 2, "Rock": 2, "Ground": 2},
-        "weaknesses": {"Electric": 2, "Grass": 2},
-        "immunities": []
-    },
-    "Fire": {
-        "strengths": {"Grass": 2, "Ice": 2, "Bug": 2, "Steel": 2},
-        "weaknesses": {"Water": 2, "Rock": 2, "Ground": 2},
-        "immunities": []
-    },
-    "Grass": {
-        "strengths": {"Water": 2, "Ground": 2, "Rock": 2},
-        "weaknesses": {"Fire": 2, "Ice": 2, "Poison": 2, "Flying": 2, "Bug": 2},
-        "immunities": []
-    },
-    "Electric": {
-        "strengths": {"Water": 2, "Flying": 2},
-        "weaknesses": {"Ground": 2},
-        "immunities": []
-    },
-    "Ice": {
-        "strengths": {"Grass": 2, "Ground": 2, "Flying": 2, "Dragon": 2},
-        "weaknesses": {"Fire": 2, "Fighting": 2, "Rock": 2, "Steel": 2},
-        "immunities": []
-    },
-    "Ground": {
-        "strengths": {"Fire": 2, "Electric": 2, "Poison": 2, "Rock": 2, "Steel": 2},
-        "weaknesses": {"Water": 2, "Ice": 2, "Grass": 2},
-        "immunities": ["Electric"]
-    },
-    "Ghost": {
-        "strengths": {"Ghost": 2, "Psychic": 2},
-        "weaknesses": {"Ghost": 2, "Dark": 2},
-        "immunities": ["Normal", "Fighting"]
-    },
-    "Normal": {
-        "strengths": {},
-        "weaknesses": {"Fighting": 2},
-        "immunities": ["Ghost"]
-    }
-}
-
 
 # print("Estos son los tipos pokemons guardados en la variable: ", tipo_elegido) # esto es porque estoy probando la difencia entre .expend y .append y asi tambien saber si lo guarda.
+
